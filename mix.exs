e0e1fcd89f5d059439a3231b647105ae35f7356b
Mick Hansen <mick.kasper.hansen@gmail.com>
2017-11-27 15:10:20 +0100

    Hex setup

--- mix.exs
+++ mix.exs
@@ -2,15 +2,21 @@ defmodule Lambda.Mixfile do
   use Mix.Project
 
   def project do
-    [app: app(),
-     version: version(),
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: app(),
+      version: version(),
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Lambda Shim",
+      source_url: "https://github.com/linkkit/lambda-elixir"
+    ]
   end
 
-  defp version, do: "0.0.1"
+  defp version, do: "1.2.0"
 
   defp app, do: :lambda
 
@@ -21,6 +27,22 @@ defmodule Lambda.Mixfile do
   defp deps do
     [
 
+    ]
+  end
+
+  defp description() do
+    "Package Elixir for Lambda and run via shim"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "lambda_shim",
+      # These are the default files included in the package
+      files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Mick Hansen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/linkkit/lambda-elixir"}
     ]
   end
 end
